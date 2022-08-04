37e51f8cb4209a657ab367fe3bbcdc4ce1c667ad
awaisshah228 <m.awaisshah228.com>
2022-08-04 11:33:01 +0500

    changed

--- btc-wallet.js
+++ btc-wallet.js
@@ -14,24 +14,23 @@ const network = bitcoin.networks.testnet //use networks.testnet for testnet
 
 // Derivation path
 // const path = `m/49'/0'/0'/0` // Use m/49'/1'/0'/0 for testnet
-const path = `m/49'/0'/0'/0` // Use m/49'/1'/0'/0 for testnet
+const path = `m/88'/0'/0'/0` // Use m/49'/1'/0'/0 for testnet
 
-// let mnemonic = bip39.generateMnemonic()
-let mnemonic='bonus upset ritual ecology history provide develop mushroom plunge august leopard payment'
+let mnemonic = bip39.generateMnemonic()
 const seed = bip39.mnemonicToSeedSync(mnemonic)
 let root = bip32.fromSeed(seed, network)
 
 let account = root.derivePath(path)
 let node = account.derive(0).derive(0)
 
-let btcAddress = bitcoin.payments.p2pkh({
-  pubkey: node.publicKey,
-  network: network,
-}).address
-// let btcAddress = bitcoin.p2wpkh({
+// let btcAddress = bitcoin.payments.p2pkh({
 //   pubkey: node.publicKey,
 //   network: network,
 // }).address
+let btcAddress = bitcoin.p2wpkh({
+  pubkey: node.publicKey,
+  network: network,
+}).address
 
 // const btcAddress = bitcoin.payments.p2wpkh({ pubkey: node.publicKey });
 // console.log(btcAddress)
