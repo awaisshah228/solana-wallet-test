3d75b4b86c734463ebf151d21b1b3f41eb4d9d09
awaisshah228 <m.awaisshah228.com>
2022-08-04 11:41:41 +0500

    added

---
+++ cardona-wallet.js
@@ -0,0 +1,9 @@
+const { Seed, WalletServer } = require('cardano-wallet-js');
+    
+let walletServer = WalletServer.init('http://you.server.com');
+let recoveryPhrase = Seed.generateRecoveryPhrase();
+let mnemonic_sentence = Seed.toMnemonicList(recoveryPhrase);
+let passphrase = 'tangocrypto';
+let name = 'tangocrypto-wallet';
+    
+let wallet = await walletServer.createOrRestoreShelleyWallet(name, mnemonic_sentence, passphrase);
